674e2c1c1b4dc832d305c72fe766492cda8ac202
Brian Whitmer <brian.whitmer@gmail.com>
2014-12-12 13:48:39 -0700

    trying to get resque working..

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,12 +3,15 @@ module RedisInit
     redis_url = ENV["REDISCLOUD_URL"] || ENV["OPENREDIS_URL"] || ENV["REDISGREEN_URL"] || ENV["REDISTOGO_URL"]
     return unless redis_url
     uri = URI.parse(redis_url)
-    if defined?(Resque)
-      Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-      Resque.redis.namespace = "open_boards"
+    begin
+      if defined?(Resque)
+        Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+        Resque.redis.namespace = "open_boards"
+      end
+      redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+      @default = Redis::Namespace.new("open_boards-stash", :redis => redis)
+    rescue => e
     end
-    redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    @default = Redis::Namespace.new("open_boards-stash", :redis => redis)
   end
   
   def self.default
