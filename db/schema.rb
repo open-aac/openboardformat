703bed9d00e07ae2043ef478745e4c647511fc13
Brian Whitmer <brian.whitmer@gmail.com>
2019-02-02 21:49:25 -0500

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20141202200534) do
 
-  create_table "progresses", force: true do |t|
+  create_table "progresses", force: :cascade do |t|
     t.text     "settings"
     t.string   "nonce"
     t.datetime "started_at"
